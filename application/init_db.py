3da05e36ec43fdc0c3a4c2228fd2dc954fc5071f
Efriem <efidesalew@gmail.com>
2020-12-01 17:02:00 +0300

    update init_db.py

--- application/init_db.py
+++ application/init_db.py
@@ -21,7 +21,7 @@ class InitDatabase(object):
                 gun_type INTEGER NOT NULL,
                 sub_type VARCHAR(30) NOT NULL,
                 FOREIGN KEY (gun_type) REFERENCES types(id));"""
-        self.sql_create_items_table = """CREATE TABLE IF NOT EXISTS items (
+        self.sql_create_items_table_1 = """CREATE TABLE IF NOT EXISTS items (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 name VARCHAR(30) NOT NULL,
                 nominal INTEGER NOT NULL,
@@ -46,6 +46,26 @@ class InitDatabase(object):
                 FOREIGN KEY (gun_type) REFERENCES types(id),
                 FOREIGN KEY (sub_type) REFERENCES sub_types(id));
                 """
+        self.sql_create_items_table = """CREATE TABLE IF NOT EXISTS items (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        name VARCHAR(30) NOT NULL,
+                        nominal INTEGER NOT NULL,
+                        mean INTEGER NOT NULL,
+                        restock INTEGER NOT NULL,
+                        life_time INTEGER NOT NULL,
+                        usage VARCHAR(30) NOT NULL,
+                        tire VARCHAR(30) NOT NULL,
+                        rarity VARCHAR(30) NOT NULL,
+                        gun_type VARCHAR(30) NOT NULL,
+                        sub_type VARCHAR(30) NOT NULL,
+                        mod VARCHAR(30) NOT NULL,
+                        trader INTEGER NOT NULL,
+                        dynamic_event INTEGER NOT NULL,
+                        count_in_cargo INTEGER NOT NULL,
+                        count_in_hoarder INTEGER NOT NULL,
+                        count_in_map INTEGER NOT NULL,
+                        count_in_player INTEGER NOT NULL);
+                        """
 
     def create_tables(self):
         # database connection
@@ -58,6 +78,17 @@ class InitDatabase(object):
         db_cursor.execute(self.sql_create_rarities_table)
         db_cursor.execute(self.sql_create_types_table)
         db_cursor.execute(self.sql_create_sub_types_table)
+        db_cursor.execute(self.sql_create_items_table)
+        # committing changes to the database
+        db_connection.commit()
+        # closing database connection
+        db_connection.close()
+
+    def create_table(self):
+        db_connection = sqlite3.connect(self.db_name)
+        # cursor
+        db_cursor = db_connection.cursor()
+        # executing sql statements for creating tables items table
         db_cursor.execute(self.sql_create_items_table)
         # committing changes to the database
         db_connection.commit()
--- application/manage_db.py
+++ application/manage_db.py
@@ -10,22 +10,29 @@ class Database(object):
     CRUD Operations related to usages
     '''
 
-    def create_usage(self, usage):
+    def create_item(self, item: Item):
         # database connection
         db_connection = sqlite3.connect(self.db_name)
         # sql statement
-        sql_create_usage = "INSERT INTO usages(usage) VALUES(?)"
+        sql_create_usage = "INSERT INTO usages(name,nominal,mean,restock,life_time,usage,tire,rarity,gun_type," \
+                           "sub_type,mod,trader,dynamic_event,count_in_cargo,count_in_hoarder,count_in_map," \
+                           "count_in_player) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) "
         # database cursor
         db_cursor = db_connection.cursor()
-        db_cursor.execute(sql_create_usage, (usage,))
+        db_cursor.execute(sql_create_usage, (item.get_name(), item.get_nominal(), item.get_mean(), item.get_restock(),
+                                             item.get_life_time(), item.get_usage(), item.get_tire(), item.get_rarity(),
+                                             item.get_type(), item.get_sub_type(), item.get_mod(), item.get_trader(),
+                                             item.get_dynamic_event(), item.get_count_in_cargo(),
+                                             item.get_count_in_hoarder(), item.get_count_in_map(),
+                                             item.get_count_in_player()))
         db_connection.commit()
         db_connection.close()
 
-    def update_usage(self, usage_id, usage):
+    def update_item(self, usage_id, usage):
         pass
 
-    def delete_usage(self, usage_id):
+    def delete_item(self, usage_id):
         pass
 
-    def all_usages(self):
+    def all_items(self):
         pass
