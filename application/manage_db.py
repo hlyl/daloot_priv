bc2f5f78a681b0909249f5ef56a7eb1c45038b83
Efriem <efidesalew@gmail.com>
2020-12-07 17:25:43 +0300

    updates made to  all_items in manage_db.py

--- application/manage_db.py
+++ application/manage_db.py
@@ -24,20 +24,7 @@ class Database(object):
 
     # update item
     def update_item(self, item: Item):
-        db_connection = sqlite3.connect(self.db_name)
-        sql_create_update = "UPDATE items set name=?,nominal=?,mean=?,restock=?,life_time=?,usage=?,tire=?,rarity=?," \
-                            "gun_type=?,sub_type=?,mod=?,trader=?,dynamic_event=?,count_in_cargo=?,count_in_hoarder=?," \
-                            "count_in_map=?,count_in_player=? WHERE id=?"
-        db_cursor = db_connection.cursor()
-        db_cursor.execute(sql_create_update, (item.get_name(), item.get_nominal(), item.get_mean(), item.get_restock(),
-                                              item.get_life_time(), item.get_usage(), item.get_tire(),
-                                              item.get_rarity(),
-                                              item.get_type(), item.get_sub_type(), item.get_mod(), item.get_trader(),
-                                              item.get_dynamic_event(), item.get_count_in_cargo(),
-                                              item.get_count_in_hoarder(), item.get_count_in_map(),
-                                              item.get_count_in_player(), item.get_item_id()))
-        db_connection.commit()
-        db_connection.close()
+        pass
 
     # get item
     def get_item(self, item_id):
@@ -47,8 +34,16 @@ class Database(object):
 
     # get items
     def all_items(self):
-        items = self.session.query(Item).all()
-        self.session.commit()
+        """items = self.session.query(Item).all()
+        self.session.commit()"""
+
+        db_connection = sqlite3.connect(self.db_name)
+        sql_delete_items = "select * from items"
+        db_cursor = db_connection.cursor()
+        db_cursor.execute(sql_delete_items)
+        items = db_cursor.fetchall()
+        db_connection.commit()
+        db_connection.close()
         return items
 
     # delete item
